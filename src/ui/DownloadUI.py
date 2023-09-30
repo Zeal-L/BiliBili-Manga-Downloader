55ec235df157ea0c04725a9b7eb3ff451960f7a2
Zeal-L <abc982210694@gmail.com>
2023-09-30 09:52:37 +0800

    :art: 去掉多余的引用；更改问题模版的描述

--- src/ui/DownloadUI.py
+++ src/ui/DownloadUI.py
@@ -4,7 +4,6 @@
 
 from __future__ import annotations
 
-import os
 from typing import TYPE_CHECKING
 
 from PySide6.QtCore import QObject, Qt, Signal
@@ -119,7 +118,9 @@ class DownloadUI(QObject):
         # ?###########################################################
         # ? 超链接打开保存路径
         label_file_path = QLabel("<a href='file:///'>打开文件夹</a>")
-        label_file_path.linkActivated.connect(lambda: openFolderAndSelectItems(mainGUI, path))
+        label_file_path.linkActivated.connect(
+            lambda: openFolderAndSelectItems(mainGUI, path)
+        )
         h_layout_download_list.addWidget(label_file_path)
 
         widget = QWidget()
