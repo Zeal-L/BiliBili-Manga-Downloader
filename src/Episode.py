ca8ac7f2405a1442a80d942f0f1a41243247c57a
spr-equinox <66425054+spr-equinox@users.noreply.github.com>
2023-11-12 10:53:26 +0800

    Update Episode.py

--- src/Episode.py
+++ src/Episode.py
@@ -74,12 +74,10 @@ class Episode:
 
         # ?###########################################################
         # ? 修复短标题中的数字
-        if re.search(r"^[0-9\-]+话", self.title):
-            self.title = re.sub(r"^([0-9\-]+)", r"第\1", self.title)
-        elif re.search(r"^[0-9\-]+\.[0-9\-]+", self.title):
-            self.title = re.sub(r"^([0-9\-]+\.[0-9\-]+)", r"第\1话", self.title)
-        elif re.search(r"^[0-9\-]+", self.title):
-            self.title = re.sub(r"^([0-9\-]+)", r"第\1话", self.title)
+        if re.search(r"^[0-9\-\.]+话", self.title):
+            self.title = re.sub(r"^([0-9\-\.]+)话", r"第\1话", self.title)
+        elif re.search(r"^[0-9\-\.]+ ", self.title):
+            self.title = re.sub(r"^([0-9\-\.]+) ", r"第\1话 ", self.title)
 
         self.headers = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
