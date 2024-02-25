3d2bc2c2b07ad02beaff186a6495dc99d4998bab
Zeal-L <abc982210694@gmail.com>
2024-02-26 02:17:24 +0800

    ✨ 添加 mac 环境下 的窗口检测（多开）

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 
 
 import ctypes
+import subprocess
 from sys import argv, exit, platform
 
 from PySide6.QtWidgets import QApplication, QMessageBox
@@ -19,6 +20,29 @@ if __name__ == "__main__":
             None, "提示", "有一个我已经不满足不了你吗？\n\t...(｡•ˇ‸ˇ•｡) ..."
         )
         exit(0)
+    elif platform == "darwin":
+        script = '''
+        set windowTitle to "{}"
+        tell application "System Events"
+            set listOfProcesses to every process whose visible is true
+            repeat with proc in listOfProcesses
+                try
+                    if exists (window 1 of proc where the name contains windowTitle) then
+                        return true
+                    end if
+                end try
+            end repeat
+        end tell
+        return false
+        '''.format(__main_window_title__)
+
+        try:
+            output = subprocess.check_output(["osascript", "-e", script], text=True).strip()
+            if output == "true":
+                QMessageBox.information(None, "提示", "有一个我已经不满足不了你吗？\n\t...(｡•ˇ‸ˇ•｡) ...")
+                exit(0)
+        except subprocess.CalledProcessError as e:
+            print("Error executing AppleScript:", e)
 
     window = MainGUI(app)
     window.show()
