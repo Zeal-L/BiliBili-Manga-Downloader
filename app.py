b83abe6a8bdbae8eba283da05a93574dc19b916f
Zeal-L <abc982210694@gmail.com>
2024-04-05 03:45:19 +0800

    👷 Add Codacy security scan workflow and fix typo in app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     if platform == "win32" and ctypes.windll.user32.FindWindowW(None, __main_window_title__) != 0:
         _box = QMessageBox.information(
-            None, "提示", "有一个我已经不满足不了你吗？\n\t...(｡•ˇ‸ˇ•｡) ..."
+            None, "提示", "有一个我已经满足不了你吗？\n\t...(｡•ˇ‸ˇ•｡) ..."
         )
         exit(0)
     elif platform == "darwin":
@@ -40,7 +40,7 @@ if __name__ == "__main__":
             output = subprocess.check_output(["osascript", "-e", script], text=True).strip()
             if output == "true":
                 QMessageBox.information(
-                    None, "提示", "有一个我已经不满足不了你吗？\n\t...(｡•ˇ‸ˇ•｡) ..."
+                    None, "提示", "有一个我已经满足不了你吗？\n\t...(｡•ˇ‸ˇ•｡) ..."
                 )
                 exit(1)
         except subprocess.CalledProcessError as e:
