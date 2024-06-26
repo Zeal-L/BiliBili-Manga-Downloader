88868afa06fd2fc131f07c302c190d858a9df00d
hyrulelinks <134026642+hyrulelinks@users.noreply.github.com>
2024-06-26 16:38:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 这是一个哔哩哔哩漫画下载器的应用程序，它提供了一个GUI界面，可以让用户下载漫画。这个文件是程序的入口文件，用于启动程序。
 """
 
-
 import ctypes
 import subprocess
 from sys import argv, exit, platform
@@ -50,5 +49,6 @@ def main():
     window = MainGUI(app)
     window.show()
     app.exec()
+    
 if __name__ == "__main__":
     main()
